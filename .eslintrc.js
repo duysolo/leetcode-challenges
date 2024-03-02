9942905cf9b10369062f065ecebf148e867fad76
Justin <duypt.dev@gmail.com>
2024-03-02 13:00:33 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+const baseConfigs = require('./node_modules/gts/.eslintrc.json')
+
+module.exports = {
+  ...baseConfigs,
+  rules: {
+    ...baseConfigs.rules,
+    semi: ['error', 'never'],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ...require('gts/.prettierrc.json'),
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+}
