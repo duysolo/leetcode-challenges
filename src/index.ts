31a3cad7cac4ffa379c37e684afa187ac40fd106
Justin <duypt.dev@gmail.com>
2024-03-09 19:11:13 +0000

    remove useless tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import {removeElement} from './2-27-remove-element/remove-element.js'
-import {merge} from './1-88-merge-sorted-array/merge-sorted-array.js'
-
-merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-
-removeElement([3, 2, 2, 3], 3)
-removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
+console.log('Hello, world!')
